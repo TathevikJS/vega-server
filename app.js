1757aca8d5933f13f45a0bb012b542a0a5c52cac
Tathevik Hayrapetyan <tatevik.h@sdc.am>
2023-11-12 00:36:29 +0400

    try

--- app.js
+++ app.js
@@ -1,63 +1,60 @@
 const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
-// const config = require('config')
-// const Post = require('./models/Post')
+const { Router, request } = require('express')
+const config = require('config')
+const path = require('path')
+const Post = require('./models/Post')
+const router = Router()
+const fs = require('fs');
 
-// app.use(cors());
-// app.use(express.json());
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", '*');
-//     res.header("Access-Control-Allow-Credentials", true);
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-//     next();
-// });
+app.use(cors({
+    origin: '*',
+    methods: 'GET, POST, PATCH, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+}));
+app.use(express.json());
 
 // MongoDB connection
-// mongoose.connect("mongodb+srv://bpoghos94:1q2w3e4r5t6y7u8i9o0p@vega.iwqfhfa.mongodb.net/architect?retryWrites=true&w=majority", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true
-// });
-
-// Set EJS as templating engine (if needed)
-// app.set("view engine", "ejs");
-
-const PORT = process.env.PORT || 27027;
-
-// Define a route for getting posts
-// app.get('/api/posts', async (req, res) => {
-//     try {
-//         // Replace 'Post' with your actual Mongoose model
-//         const posts = await Post.find();
-//         res.json(posts);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: 'Something went wrong' });
-//     }
-// });
-
-app.get('/', async (req, res) => {
-    res.send('Hello World!');
+mongoose.connect("mongodb+srv://bpoghos94:1q2w3e4r5t6y7u8i9o0p@vega.iwqfhfa.mongodb.net/architect?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
 });
 
-// // Define a route for creating a post
-// app.post('/api/posts/create', async (req, res) => {
-//     console.log(req.body, 'REQREQRE');
-//     try {
-//         const newPost = new Post({
-//             title: req.body.title,
-//         });
-//         await newPost.save();
-//         res.status(201).json({ post: newPost });
-//         console.log('saved');
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json(error, { message: 'Something went wrong' });
-//     }
-// });
+// Set EJS as templating engine (if needed)
+app.set("view engine", "ejs");
+
+const PORT = process.env.PORT || 27017;
+
+// Define a route for getting posts
+app.get('/api/posts', async (req, res) => {
+    try {
+        // Replace 'Post' with your actual Mongoose model
+        const posts = await Post.find();
+        res.json(posts);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Something went wrong' });
+    }
+});
+
+// Define a route for creating a post
+app.post('/api/posts/create', async (req, res) => {
+    console.log(req.body, 'REQREQRE');
+    try {
+        const newPost = new Post({
+            title: req.body.title,
+        });
+        await newPost.save();
+        res.status(201).json({ post: newPost });
+        console.log('saved');
+    } catch (error) {
+        console.error(error);
+        res.status(500).json(error, { message: 'Something went wrong' });
+    }
+});
 
 // app.patch('/update/:id', async (req, res) => {
 //     console.log(req.body, 'REQREQRE');
@@ -81,19 +78,19 @@ app.get('/', async (req, res) => {
 // })
 
 
-// app.get('/api/posts/:id', async (req, res) => {
-//     const {id} = req.params;
-//     try {
-//         const data = await Post.findById(id);
-//         console.log(data);
-//         if (!data) {
-//             return res.status(404).json({ error: 'Post not found' });
-//         }
-//         res.json(data); // 
-//     } catch (error) {
-//         res.status(400).json({ error: "Something went wrong" }); // Return JSON error response
-//     }
-// });
+app.get('/api/posts/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const data = await Post.findById(id);
+        console.log(data);
+        if (!data) {
+            return res.status(404).json({ error: 'Post not found' });
+        }
+        res.json(data); // 
+    } catch (error) {
+        res.status(400).json({ error: "Something went wrong" }); // Return JSON error response
+    }
+});
 
 
 app.delete('/api/posts/del/:id', async(req,res)=>{
